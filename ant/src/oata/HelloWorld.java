69772c007b67ba5178b8effa48a0d9318803a60f
Masato Oikawa <masato.oikawa@upstairs.jp>
2026-04-08 22:25:28 +0900

    Add test

--- ant/src/oata/HelloWorld.java
+++ ant/src/oata/HelloWorld.java
@@ -2,8 +2,10 @@ package oata;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        String s = null;
-	s.length();
         System.out.println("Hello World");
+    }
+
+    public static int triggerForwardedNullDeref(String value) {
+        return value.length();
     }
 }
